ef4988e194bec2cc3aee0e251e664d68f49fe4e9
Furqan <furqode@gmail.com>
2023-04-27 01:52:47 +0100

    add title and a logout button to the page

---
+++ index.php
@@ -0,0 +1,23 @@
+<?php  
+
+?>
+
+
+<!DOCTYPE html>
+<html>
+    
+    <head>
+        <title>Website</title>
+    </head>
+    
+    <body>
+        <a href="logout.php">Logout</a>
+        <h1>This is the index page.</h1>
+        
+        <br>
+
+            Hello, Username.
+    </body>
+
+
+</html>
