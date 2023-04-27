4864288d7a92d3ca65f0898affeb9764e6cf80bb
Furqan <furqode@gmail.com>
2023-04-27 02:05:38 +0100

    add form to the page

---
+++ login.php
@@ -0,0 +1,49 @@
+<?php
+
+?>
+
+<html>
+    <head>
+        <title>Login</title>
+    </head>
+    <body>
+        <style>
+            #text{
+
+                height: 25px;
+                border-radius: 5px;
+                padding: 4px;
+                border: solid thin #aaa;
+                width: 100%;
+            }
+
+            #button{
+
+                padding: 10px;
+                width: 100px;
+                color: white;
+                background-color: lightblue;
+                border: none;
+            }
+
+            #box{
+
+                background-color: grey;
+                margin: auto;
+                width: 300px;
+                padding: 20px;
+            }
+        </style>
+    <div id="box">
+        <form action=" " method="post">
+            <div style="font-size:20px; margin:10px;">Login</div>
+            <input type="text" name="user_name"><br><br>
+            <input type="password" name="password"><br><br>
+
+            <input type="submit" value="Login"><br><br>
+
+            <a href="signup.php ">Signup</a><br><br>
+        </form>
+    </div>    
+</body>
+</html>
