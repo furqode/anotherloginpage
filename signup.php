bd2773da169768a419ba7bb6a852cbeeba877408
Furqan <furqode@gmail.com>
2023-04-29 04:18:12 +0100

    if the input data is valid it is stored and user is redirected to the login page

--- signup.php
+++ signup.php
@@ -2,6 +2,28 @@
 session_start();
 include("connection.php");
 include("functions.php");
+
+if ($_SERVER['REQUEST_METHOD'] == "POST") {
+    
+    //something was posted
+    $user_name = $_POST['user_name'];
+    $password = $_POST['password'];
+
+    if(!empty($user_name) && !empty($password) && !is_numeric($user_name)) {
+        
+        //save to data base
+        $user_id = random_num(20);
+        $query = "insert into users (user_id, user_name, password) values ('$user_id', '$user_name', '$password')";
+
+        mysqli_query($query);
+        
+        header("Location: login.php");
+        die;
+    }
+    else {
+        echo "Please enter some valid information";
+    }
+}
 ?>
 
 <html>
