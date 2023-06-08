22d8f56e00f348549eaff33a77c2ea8b54418446
Furqan <furqode@gmail.com>
2023-06-08 23:29:38 +0100

    fix the error with random number function on line 37

--- functions.php
+++ functions.php
@@ -27,14 +27,14 @@ function random_num($length) {
     $text = "";
 
     if ($length < 5 ) {
-        $length = 5
+        $length = 5;
     }
 
     $len = rand(4, $length);
 
     for ($i=0; $i < $len ; $i++) { 
         # code...
-        $text .= (0, 9);
+        $text .= rand(0,9);
     }
     return $text;
 }
